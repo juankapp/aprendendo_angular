933868f731cfaf5658d2ad08a52927e155411b2d
Juan <juanauer2019@gmail.com>
2023-11-15 11:46:36 -0300

    renderizacao_condicional

--- src/app/component/renderizacao-condicional/renderizacao-condicional.component.ts
+++ src/app/component/renderizacao-condicional/renderizacao-condicional.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class RenderizacaoCondicionalComponent {
 
+  //*ngIf é como se fosse um if, caso o valor seja boolean  exibe 
+  //pra utilizar o else, deve *ngIf="validacao else nãoValidado", assim é a sintaxe
+  //depois cria uma tag  <ng-template #nãoValidado>, e coloca #nãoValidado, se referindo ao else 
+
+  usuarioLogado: boolean = true;
+  nomeUsuario: string = 'Juan';
+
+  
+
+
 }
